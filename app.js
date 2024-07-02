698f0905c5ae491e976cfdda0dc0eef1a0051f37
Chibuzo Ekwue <chieezy5@gmail.com>
2024-07-02 22:40:30 +0100

    minor touch

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ import express from "express";
 
 const app = express();
 
-app.use("trust proxy", true);
+app.use(express.json());
+
+app.set("trust proxy", true);
 
 const port = process.env.PORT || 1000;
 
 app.get("/", async (req, res) => {
 	const { visitor_name } = req.query;
 
-	const ip = req.ip;
 	try {
 		const { data } = await axios(
-			`https://api.geoapify.com/v1/ipinfo?ip=${ip}&apiKey=${process.env.API_KEY}`
+			`https://api.geoapify.com/v1/ipinfo?apiKey=${process.env.API_KEY}`
 		);
-		const { city, location, ip } = data;
+		const { city, location,ip } = data;
 
 		const { data: weatherInfo } = await axios(
 			`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.WEATHER_KEY}&units=metric`
@@ -23,14 +24,12 @@ app.get("/", async (req, res) => {
 
 		res.json({
 			client_ip: ip,
-
 			location: city.name,
 			greeting: `Hello, ${visitor_name || ""}! the temperature is ${
 				weatherInfo.main.temp
 			} Celcius in ${city.name}`,
 		});
 	} catch (error) {
-		console.log(error);
 		res.status(400).json({ error: error.message || "Something went bad" });
 	}
 });
